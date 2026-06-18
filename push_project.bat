c7b5df4c33c37001ee4bdcbea9442651b6c36d4e
UpSkill Developer <developer@upskill.com>
2026-06-18 05:08:11 -0700

    Initial commit

---
+++ push_project.bat
@@ -0,0 +1,14 @@
+@echo off
+title UpSkill GitHub Pusher
+echo ===================================================
+echo 🚀 UpSkill Project GitHub Pusher
+echo ===================================================
+echo.
+echo Pushing project to: https://github.com/nagisettijhansisrikavya-hub/CSP-Project-Upskill-
+echo.
+echo Running git push using portable Git...
+".git_portable\cmd\git.exe" push -u origin main
+echo.
+echo ===================================================
+echo Finished. Press any key to close this window.
+pause
